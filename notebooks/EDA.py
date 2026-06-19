1689836a9a3cabf6ad2413a14e67407f595cbd2b
s-u-priya <devisupriya6811@gmail.com>
2026-06-19 21:10:00 +0530

    Update EDA.py

--- notebooks/EDA.py
+++ notebooks/EDA.py
@@ -1,1 +1,20 @@
+import pandas as pd
 
+df = pd.read_csv("data/telco.csv")
+
+df["TotalCharges"] = (
+    df["TotalCharges"]
+    .replace(" ", "0")
+)
+
+df["TotalCharges"] = pd.to_numeric(
+    df["TotalCharges"]
+)
+
+charges_churn = df.groupby(
+    "Churn"
+)["MonthlyCharges"].mean()
+
+print(
+    charges_churn.round(2)
+)
