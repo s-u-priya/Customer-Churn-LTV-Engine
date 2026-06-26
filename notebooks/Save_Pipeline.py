f1769df917eacd284c477cd03a6bb73557aebf9a
s-u-priya <devisupriya6811@gmail.com>
2026-06-26 19:17:33 +0530

    Update Save_Pipeline.py

--- notebooks/Save_Pipeline.py
+++ notebooks/Save_Pipeline.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import joblib
-
 from sklearn.model_selection import train_test_split
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder
